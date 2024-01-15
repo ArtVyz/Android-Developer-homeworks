ba1be9b4754749259b969ec1a40e1c240f44fcc1
Artem Vyazov <aad-93@mail.ru>
2024-01-15 19:17:08 +0700

    HomeWork5 is ready

--- m5_quiz_resources/app/src/main/java/com/example/homework5/MainActivity.kt
+++ m5_quiz_resources/app/src/main/java/com/example/homework5/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.homework5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.skillbox_hw_quiz.R
+import com.example.homework5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
