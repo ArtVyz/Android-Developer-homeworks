6276e296b57eea3dc0b780336f326d886535307b
ArtemVyazov <aad-93@mail.ru>
2024-04-04 23:29:30 +0700

    HomeWork8 not ready

---
+++ m8_quiz_animation/app/src/main/java/com/example/homework5/ResultFragment.kt
@@ -0,0 +1,38 @@
+package com.example.homework5
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.homework5.databinding.FragmentResultBinding
+
+
+class ResultFragment : Fragment() {
+
+    private var _binding: FragmentResultBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentResultBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    val args: ResultFragmentArgs by navArgs()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val answers = args.MyArg
+
+        binding.feedback.text = answers
+
+        binding.buttonStartAgain.setOnClickListener {
+
+            findNavController().navigate(R.id.action_resultFragment_to_surveyFragment)
+        }
+
+    }
+}
